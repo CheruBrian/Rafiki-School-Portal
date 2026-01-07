7abf5594d10689ef05fc1e909e7e681b98d33c41
Brian Cheru <cheruiyotbrian2000@gmail.com>
2026-01-07 09:46:24 +0300

    addded ChickenProject file

---
+++ chicken_project/src/components/ChickenProject.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ChickenProject() {
+    return (
+        <div>
+            <h1>Welcome to the Chicken Project</h1>
+        </div>
+    );
+}
+
+export default ChickenProject;
