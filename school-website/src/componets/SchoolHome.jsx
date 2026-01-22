eca4ed40d0442098c30db3824143c8270014edea
Brian Cheru <cheruiyotbrian2000@gmail.com>
2026-01-22 18:06:25 +0300

    updated School Home

--- school-website/src/componets/SchoolHome.jsx
+++ school-website/src/componets/SchoolHome.jsx
@@ -16,8 +16,32 @@ function SchoolHome() {
       </ul>
     </div>
     <div className="school-body">
+      
+      <div className="our-mission">
       <h2>Our Mission</h2>
-      <h2>Upcoming News</h2>
+      </div>
+      
+      <div className="upcoming-news">
+       <h2>Upcoming News</h2>
+      </div>
+      
+      <div className="contact-us">
+        <h2>Contact Us</h2>
+      <p>For any inquiries, please contact us through the form below:</p>
+            <form name="contact-us-form">
+                <label>
+                    Name:
+                    <input type="name" placeholder=" " minLength={3} maxLength={13}></input>
+                    Email:
+                    <input type="email" placeholder=" " minLength={4} maxLength={40}></input>
+                    Phone number:
+                    <input type="tel" placeholder=" " minLength={9} maxLength={20}></input>
+                    <button type="Submit"></button>
+                </label>
+            </form>
+
+      </div>
+      
     </div>
     <div className="class-footer">
         <p>&copy; 2026 Our School all rights reserved</p>
