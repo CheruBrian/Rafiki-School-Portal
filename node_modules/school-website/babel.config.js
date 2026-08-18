350a25defcb07b0caae8294bb970a36fb6e73e17
Brian Cheru <cheruiyotbrian2000@gmail.com>
2026-08-18 19:36:05 +0300

    fixing bugs

---
+++ node_modules/school-website/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+  // Optionally add plugins for features like class properties
+};
